b70ac982553e8ba94d3267aa75b5e24a4c4b7a6f
robertjbennett99 <robertjb@live.unc.edu>
2020-11-22 18:08:25 -0500

    ljlkj

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// var port = process.env.PORT || 5000
-// var express = require('express')
-// var app = express();
-
-// app.use(express.static(__dirname))
-
-// app.listen(port, function()  {
-//     console.log("frontend up and running")
-// })
-
-
-
 
 let loggedin = false; // TEMP
 
