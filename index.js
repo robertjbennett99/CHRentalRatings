427ca9477ae0a2b305bba05772fb58222692d4e2
robertjbennett99 <robertjb@live.unc.edu>
2020-11-22 21:32:59 -0500

    divided index onto login.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const express = require('express');
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,56 +12,5 @@ app.listen(port, function(){
     console.log('Frontend up and running on port ' + port)
 })
 
-
-
-let loggedin = false; // TEMP
-
-async function login() {
-    let username = document.getElementById("username_field").value;
-    let userPassword = document.getElementById("password_field").value;
-    let success = false
-    
-    try {
-        success = await axios({
-            method: 'post',
-            url: 'https://zrdj-stocksentiments.herokuapp.com/login',
-            //url: 'http://localhost:3030/login',
-            withCredentials: false,
-            headers: {"Access-Control-Allow-Origin": "*"},
-            data: {
-                "username": username,
-                "password": userPassword
-            },
-        })
-    } catch (error) {
-        alert('invalid username')
-        return
-    }
-
-    if(success.data) {
-        loggedin = true
-        loginStateChange()
-    }
-    else { alert('Invalid Password ')}
-    return;
-}
-
-async function logout(){
-    loggedin = false;
-    loginStateChange()
-
-    // REPLACE FUNCTIONALITY
-
-}
-
-function loginStateChange() {
-    if (loggedin) {
-        document.getElementById("user_div").style.display = "block";
-        document.getElementById("login_div").style.display = "none";
-      } else {
-        document.getElementById("user_div").style.display = "none";
-        document.getElementById("login_div").style.display = "block";
-      }
-};
 
 
---
+++ login.js
@@ -0,0 +1,49 @@
+let loggedin = false; // TEMP
+
+async function login() {
+    let username = document.getElementById("username_field").value;
+    let userPassword = document.getElementById("password_field").value;
+    let success = false
+    
+    try {
+        success = await axios({
+            method: 'post',
+            url: 'https://zrdj-stocksentiments.herokuapp.com/login',
+            //url: 'http://localhost:3030/login',
+            withCredentials: false,
+            headers: {"Access-Control-Allow-Origin": "*"},
+            data: {
+                "username": username,
+                "password": userPassword
+            },
+        })
+    } catch (error) {
+        alert('invalid username')
+        return
+    }
+
+    if(success.data) {
+        loggedin = true
+        loginStateChange()
+    }
+    else { alert('Invalid Password ')}
+    return;
+}
+
+async function logout(){
+    loggedin = false;
+    loginStateChange()
+
+    // REPLACE FUNCTIONALITY
+
+}
+
+function loginStateChange() {
+    if (loggedin) {
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+      } else {
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("login_div").style.display = "block";
+    }
+}
