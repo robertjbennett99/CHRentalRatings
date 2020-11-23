058e034fe2e5b96101ca53abf069b6c82410008f
robertjbennett99 <robertjb@live.unc.edu>
2020-11-22 21:16:39 -0500

    exp app

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // const express = require('express');
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname))
+
+app.get("/", function(req, res) {
+    res.render("index")
+});
+
+app.listen(port, function(){
+    console.log('Frontend up and running on port ' + port)
+})
+
+
 
 let loggedin = false; // TEMP
 
