e5fa27dfe274d384a0a20b30f09f0a06a68458d0
Jack Furey <jfurey14@live.unc.edu>
2020-11-23 00:51:32 -0500

    commit

--- login.js
+++ login.js
@@ -43,16 +43,20 @@ async function logout(){
 
 }
 
+var counter = 0;
+
 function loginStateChange() {
     if (loggedin) {
         document.getElementById("user_div").style.display = "block";
         document.getElementById("login_div").style.display = "none";
+        if(counter == 0) {
+            getTickers();
+        }
+        counter++;
       } else {
         document.getElementById("user_div").style.display = "none";
         document.getElementById("login_div").style.display = "block";
     }
-
-    getTickers();
 }
 
 let getTickers = async function() {
